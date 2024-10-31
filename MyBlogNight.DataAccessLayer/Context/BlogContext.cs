77abe6d14f61bb587d4d410a28e52ebb3ce02da6
Elif Yıldırım <elifyildirim3@outlook.com>
2024-10-31 22:14:43 +0300

    Yeni eklediğim kısımlar

--- MyBlogNight.DataAccessLayer/Context/BlogContext.cs
+++ MyBlogNight.DataAccessLayer/Context/BlogContext.cs
@@ -4,6 +4,10 @@ using MyBlogNight.EntityLayer.Concrete;
 namespace MyBlogNight.DataAccessLayer.Context;
 public class BlogContext : DbContext
 {
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer("Server=NETCADYAZ; initial catalog=BlogFoodyDb;integrated security= true;");
+    }
     public DbSet<Article> Articles { get; set; }
     public DbSet<Category> Categories { get; set; }
     public DbSet<Comment> Comments { get; set; }
--- MyBlogNight.EntityLayer/Concrete/Comment.cs
+++ MyBlogNight.EntityLayer/Concrete/Comment.cs
@@ -1,11 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace MyBlogNight.EntityLayer.Concrete;
+﻿namespace MyBlogNight.EntityLayer.Concrete;
 public class Comment
 {
+    public int Id { get; set; }
+
 }
 
